aec36ad6ae0ab14748d3290482a16abe9bef19f9
William Franklin <emailwilliamfranklin@gmail.com>
2022-06-04 12:18:25 -0700

    Updated

--- farkle.py
+++ farkle.py
@@ -1,7 +1,6 @@
 import random
 import math
 
-
 initRoll = 6
 
 roll = []
@@ -9,3 +8,13 @@ for item in range(initRoll):
     roll.append(random.randint(1,6))
 
 print (roll)
+
+class player:
+    def __init__(self, name, score, roll):
+        self.name = name
+        self.score = score
+        self.roll = roll
+
+a = input()
+
+print (type(a))
